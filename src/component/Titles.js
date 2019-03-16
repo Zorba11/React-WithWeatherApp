146fbb804fa2b7f9af45c0eb9b28bc0715935918
zorba11 <zorbathegreek111@gmail.com>
2019-03-16 18:12:01 -0400

    api fetching success and functionality ready, need styling

---
+++ src/component/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1>Weather Finder</h1>
+    <p>Find Out Temperature Conditions and more...</p>
+  </div>
+);
+
+export default Titles;
